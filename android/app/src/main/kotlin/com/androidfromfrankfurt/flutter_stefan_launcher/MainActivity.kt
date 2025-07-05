998162299bf14e1adef11f225d6ec4b90a7d5788
depidsvy <spidgorny@gmail.com>
2025-07-05 23:08:02 +0200

    feat: menu and star on the app list

---
+++ android/app/src/main/kotlin/com/androidfromfrankfurt/flutter_stefan_launcher/MainActivity.kt
@@ -0,0 +1,66 @@
+package com.androidfromfrankfurt.flutter_stefan_launcher
+
+import android.annotation.TargetApi
+import android.content.Intent
+import android.net.Uri
+import android.os.Build
+import android.os.Bundle
+import android.provider.Settings
+import android.widget.Toast
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.android.FlutterActivityLaunchConfigs.BackgroundMode.transparent
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugin.common.MethodChannel
+
+class MainActivity : FlutterActivity() {
+    private val CHANNEL = "com.androidfromfrankfurt.flutter_stefan_launcher/my_platform_service"
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        intent.putExtra("background_mode", transparent.toString())
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
+        super.configureFlutterEngine(flutterEngine)
+        MethodChannel(flutterEngine.dartExecutor.binaryMessenger, CHANNEL).setMethodCallHandler {
+            // This block is executed when a method is invoked from Flutter
+                call, result ->
+            if (call.method == "openAppInfo") {
+                val packageName = call.argument<String>("packageName")
+//                val param2 = call.argument<Int>("param2")
+
+                // Call your actual Kotlin function here
+                val dataFromKotlin = openAppInfo(packageName)
+
+                // Send the result back to Flutter
+                result.success(dataFromKotlin)
+            } else {
+                // If the method is not recognized, indicate it's not implemented
+                result.notImplemented()
+            }
+        }
+    }
+
+    // Your actual Kotlin function
+    @androidx.annotation.RequiresApi(Build.VERSION_CODES.GINGERBREAD)
+    private fun openAppInfo(packageName: String?): String {
+        // Perform some native Android operations or calculations
+        val message = "Hello from Kotlin! Received: text=$packageName"
+//        Toast.makeText(this, message, Toast.LENGTH_SHORT).show() // Example: show a Toast
+        // `context` here refers to the `Context` of the `MainActivity`.
+        // `FlutterActivity` (which `MainActivity` extends) is a subclass of `ComponentActivity`,
+        // which in turn is a subclass of `ContextThemeWrapper`, which itself is a `Context`.
+        val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
+        val uri = Uri.fromParts("package", packageName, null)
+        intent.data = uri
+        this.startActivity(intent)
+        return message
+    }
+
+    // Example of calling a Flutter function from Kotlin (bi-directional communication)
+    fun sendEventToFlutter(eventName: String, data: Any?) {
+        MethodChannel(flutterEngine!!.dartExecutor.binaryMessenger, CHANNEL)
+            .invokeMethod("onKotlinEvent", mapOf("eventName" to eventName, "data" to data))
+    }
+
+}
